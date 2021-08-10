a841b9f3be4494c7b9cebe306d48b9f195a9998e
nayan029 <nayan.vhits@gmail.com>
2021-08-10 05:47:28 -0700

    lebel change in location

--- resources/views/admin/location/index.blade.php
+++ resources/views/admin/location/index.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark"><small>Location Management</small></h1>
+                    <h1 class="m-0 text-dark"><small>Manage Location</small></h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6 mt-2">
                     <ol class="breadcrumb float-sm-right">
